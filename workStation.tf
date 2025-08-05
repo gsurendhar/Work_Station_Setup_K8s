e3646ff2813f7d46b3742f2f3884414e3de2bb37
gsurendhar <surendharg123@gmail.com>
2025-08-05 14:14:55 +0530

    EC2 and SG for workstation

---
+++ workStation.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "k8s_workstation" {
+    ami = data.aws_ami.devops.id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow-all.id]
+    user_data = file("bootstrap.sh")
+   
+    tags    = {
+        Name = "roboshop-dev"
+        Project = "Roboshop"
+    }
+}
+
+resource "aws_security_group" "allow-all" {
+    name = "allow-all-workstation"
+    description = " allowing all ports from internet "
+    ingress {
+        from_port   =   0
+        to_port     =   0
+        protocol    =   "-1"
+        cidr_blocks =   ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+        from_port   =   0
+        to_port     =   0
+        protocol    =   "-1"
+        cidr_blocks =   ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags    = {
+        Name = "workstation"
+        Project = "Roboshop"
+    }
+}
