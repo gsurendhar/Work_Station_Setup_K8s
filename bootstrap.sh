2e58789e6fd63b03cbf265f4d7ddd0c99a577ac8
gsurendhar <surendharg123@gmail.com>
2025-08-05 14:13:40 +0530

    docker ekcctl kubectl installation file

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Docker Installation
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start and Add permission to Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+usermod -aG docker ec2-user
+
+# eksctl Installation
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+#kubectl Installation
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
